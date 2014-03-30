189bff963a1b1d9e5b966cb8b201a791d661f948
Gids <gidsgoldberg@gmail.com>
2014-03-30 20:13:35 +0100

    add assertion for list all

--- src/test/java/ListAll.java
+++ src/test/java/ListAll.java
@@ -1,24 +1,45 @@
-import cucumber.api.PendingException;
+import PageObjects.SignUpPage;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import common.SharedDriver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ListAll {
+    private final WebDriver driver;
+    protected Config conf;
+    protected String emailAddress=("gids-"+ UUID.randomUUID()+"@example.org");
+    SignUpPage page;
+
+
+    public ListAll(SharedDriver driver) {
+        this.driver = driver;
+        conf = ConfigFactory.load();
+
+    }
     @Given("^I have signed up to the email list$")
-    public void iHaveSignedUpToTheEmailList() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void iHaveSignedUpToTheEmailList() {
+        driver.get(conf.getString("moo-hw.host")+"signup");
+        page = PageFactory.initElements(driver, SignUpPage.class);
+        page.SignUp(emailAddress);
     }
 
     @When("^I load the list subscribers page$")
-    public void iLoadTheListSubscribersPage() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void iLoadTheListSubscribersPage()  {
+        driver.get(conf.getString("moo-hw.host")+"all");
     }
 
     @Then("^I should see my email address$")
     public void iShouldSeeMyEmailAddress() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    assertThat(driver.findElement(By.tagName("body")).getText(), containsString(emailAddress));
     }
 }
